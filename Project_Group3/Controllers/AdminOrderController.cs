ae071abab4d5eff3c35fb0f81d86846b5f4a2a1b
longdtdhl <longdtdhl@gmail.com>
2026-03-16 14:04:12 +0700

    Done OrderManagement

---
+++ Project_Group3/Controllers/AdminOrderController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class AdminOrderController : Controller
+{
+    private readonly IOrderRepository _orderRepo;
+
+    public AdminOrderController(IOrderRepository orderRepo)
+    {
+        _orderRepo = orderRepo;
+    }
+
+    public IActionResult Index()
+    {
+        var orders = _orderRepo.GetAllOrders();
+        return View(orders);
+    }
+
+    public IActionResult Details(int id)
+    {
+        var order = _orderRepo.GetOrderById(id);
+        var items = _orderRepo.GetOrderItems(id);
+
+        ViewBag.Items = items;
+
+        return View(order);
+    }
+
+    [HttpPost]
+    public IActionResult UpdateStatus(int id, string status)
+    {
+        _orderRepo.UpdateOrderStatus(id, status);
+
+        return RedirectToAction("Details", new { id = id });
+    }
+}
---
+++ Project_Group3/Repository/OrderRepository.cs
@@ -0,0 +1,43 @@
+﻿using Project_Group3.Models;
+
+public class OrderRepository : IOrderRepository
+{
+    private readonly CloneEbayDbContext _context;
+
+    public OrderRepository(CloneEbayDbContext context)
+    {
+        _context = context;
+    }
+
+
+    public List<OrderTable> GetAllOrders()
+    {
+        return _context.OrderTables
+            .OrderByDescending(o => o.orderDate)
+            .ToList();
+    }
+
+    public OrderTable GetOrderById(int id)
+    {
+        return _context.OrderTables
+            .FirstOrDefault(o => o.id == id);
+    }
+
+    public List<OrderItem> GetOrderItems(int orderId)
+    {
+        return _context.OrderItems
+            .Where(o => o.orderId == orderId)
+            .ToList();
+    }
+
+    public void UpdateOrderStatus(int id, string status)
+    {
+        var order = _context.OrderTables.FirstOrDefault(o => o.id == id);
+
+        if (order != null)
+        {
+            order.status = status;
+            _context.SaveChanges();
+        }
+    }
+}
