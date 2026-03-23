df806b26d87a4ff587fc3013683c005aa05ce25e
nguyenvyanh <nguyenvyanh040805@gmail.com>
2026-03-24 05:41:24 +0700

    2FA

---
+++ Project_Group3/Models/AdminTwoFactorSettingsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project_Group3.Models;
+
+public sealed class AdminTwoFactorSettingsViewModel
+{
+    [Display(Name = "Enable 2FA")]
+    public bool IsTwoFactorEnabled { get; set; }
+
+    [Display(Name = "Current password")]
+    public string? CurrentPassword { get; set; }
+
+    public string? Email { get; set; }
+
+    public string Role { get; set; } = string.Empty;
+}
